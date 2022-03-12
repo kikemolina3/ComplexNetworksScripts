d60157ecf5383f3fad48ecddfe0026e38f738342
AT017314 <Enrique.Molina@idiada.com>
2022-03-12 22:28:24 +0100

    doc & revision

--- main.py
+++ main.py
@@ -2,30 +2,32 @@ import csv
 import networkx as nx
 import os, os.path, sys
 import numpy
+import statistics
 
-# FILE1HEADER = ['Graph name', 'Number of nodes', 'Number of edges', 'Minimum degree', 'Maximum degree', 'Average degree', 'Avg clustering coefficient', 'Assortativity', 'Average path length', 'Diameter']
-# GENERALIST = []
-# for folder in os.listdir(os.getcwd() + '\\exercise1'):
-#     for net in os.listdir(os.getcwd() + "\\exercise1\\" + folder):
-#         GRAPHLIST = []
-#         g = nx.read_pajek(os.getcwd() + "\\exercise1\\" + folder + "\\" + net)
-#         g = nx.Graph(g)
-#         GRAPHLIST.append(net)
-#         GRAPHLIST.append(str(nx.number_of_nodes(g)))
-#         GRAPHLIST.append(str(nx.number_of_edges(g)))
-#         degrees = [val for (node, val) in g.degree()]
-#         GRAPHLIST.append(str(max(degrees)))
-#         GRAPHLIST.append(str(min(degrees)))
-#         GRAPHLIST.append(str(sum(degrees)/float(len(degrees))))
-#         GRAPHLIST.append(str(nx.average_clustering(g)))
-#         GRAPHLIST.append(str(nx.degree_assortativity_coefficient(g)))
-#         GRAPHLIST.append(str(nx.average_shortest_path_length(g)))
-#         GRAPHLIST.append(str(nx.average_shortest_path_length(g)))
-#         GENERALIST.append(GRAPHLIST)
-# with open('part1.csv', 'w', newline='') as csvfile:
-#     csvwriter = csv.writer(csvfile)
-#     csvwriter.writerow(FILE1HEADER)
-#     csvwriter.writerows(GENERALIST)
+### PART 1 - DESCRIPTORS OF GRAPHS
+FILE1HEADER = ['Graph name', 'Number of nodes', 'Number of edges', 'Minimum degree', 'Maximum degree', 'Average degree', 'Avg clustering coefficient', 'Assortativity', 'Average path length', 'Diameter']
+GENERALIST = []
+for folder in os.listdir(os.getcwd() + '\\exercise1'):
+    for net in os.listdir(os.getcwd() + "\\exercise1\\" + folder):
+        GRAPHLIST = []
+        g = nx.read_pajek(os.getcwd() + "\\exercise1\\" + folder + "\\" + net)
+        g = nx.Graph(g)
+        GRAPHLIST.append(net)
+        GRAPHLIST.append(str(nx.number_of_nodes(g)))
+        GRAPHLIST.append(str(nx.number_of_edges(g)))
+        degrees = [val for (node, val) in g.degree()]
+        GRAPHLIST.append(str(min(degrees)))
+        GRAPHLIST.append(str(max(degrees)))
+        GRAPHLIST.append(str(sum(degrees)/float(len(degrees))))
+        GRAPHLIST.append(str(nx.average_clustering(g)))
+        GRAPHLIST.append(str(nx.degree_assortativity_coefficient(g)))
+        GRAPHLIST.append(str(nx.average_shortest_path_length(g)))
+        GRAPHLIST.append(str(nx.diameter(g)))
+        GENERALIST.append(GRAPHLIST)
+with open('part1.csv', 'w') as csvfile:
+    csvwriter = csv.writer(csvfile)
+    csvwriter.writerow(FILE1HEADER)
+    csvwriter.writerows(GENERALIST)
 
 ### PART 2 - DESCRIPTORS OF NODES IN AIRPORTS NET
 g = nx.read_pajek('exercise1/real/airports_UW.net')
@@ -50,7 +52,7 @@ for name in nx.nodes(g):
     STRENGTHS.append(str(nx.degree(gW, name, weight='weight')))
     CLUSTRCOFF.append(str(nx.clustering(g,name)))
     length_dict = nx.single_source_shortest_path_length(g, name)
-    AVGPHLNG.append(str(sum(list(length_dict.values()))/float(len(length_dict))))
+    AVGPHLNG.append(str(statistics.mean(length_dict.values())))
     MAXPHLNG.append(str(max(list(length_dict.values()))))
 GENERALIST = [NAMES, DEGREES, STRENGTHS, CLUSTRCOFF, AVGPHLNG, MAXPHLNG, BETWEENNESS, EIGENVECTORS, PAGERANK]
 GENERALIST=list(map(list, zip(*GENERALIST)))
