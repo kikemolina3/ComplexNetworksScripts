19a0c6231d80396ce218b4e2d99909ff502edada
AT017314 <Enrique.Molina@idiada.com>
2022-03-26 17:10:33 +0100

    first steps exercise2

--- exercise2/main.py
+++ exercise2/main.py
@@ -26,17 +26,17 @@ def ER_graph(N, k):
 
 if __name__=='__main__':
 # -------------- ER networks ---------------------
-    for N in N_list:
-        max_k=N*(N-1)/2
-        list_k=[max_k/(N*2), max_k/N, max_k*2/N]
-        for k in list_k:
-            g = ER_graph(N, k)  
-            pos=nx.random_layout(g) 
-            nx.draw(g,pos,node_size=factor/N)
-            plt.show()
+    # for N in N_list:
+    #     max_k=N*(N-1)/2
+    #     list_k=[max_k/(N*2), max_k/N, max_k*2/N]
+    #     for k in list_k:
+    #         g = ER_graph(N, k)  
+    #         pos=nx.random_layout(g) 
+    #         nx.draw(g,pos,node_size=factor/N)
+    #         plt.show()
 
 # --------------- BA networks --------------------
-    list=[{'N': 100, 'm0': 10, 'm':4},{'N': 500, 'm0': 50, 'm':10},{'N': 1000, 'm0': 100, 'm':13}]
+    list=[{'N': 100, 'm0': 10, 'm':1},{'N': 500, 'm0': 50, 'm':1},{'N': 1000, 'm0': 100, 'm':1}]
     for dict in list:
         #GENERATE RANDOM GRAPH
         N=dict.get('N')
@@ -44,8 +44,10 @@ if __name__=='__main__':
         m=dict.get('m')
         K=m0*(m0-1)*20/N
         g=ER_graph(m0,K)
+        largest_component = max(nx.connected_components(g), key=len)
+        g = nx.Graph(g.subgraph(largest_component))
         #BEGIN TO GROW THE GRAPH
-        for node_to_add in range(m0, N):
+        for node_to_add in range(m0-1, N):
             for edge in range (0,m):
                 while True:
                     nodes = [node for (node, val) in g.degree()]
@@ -53,12 +55,11 @@ if __name__=='__main__':
                     suma=sum(degrees)
                     degrees=np.array(degrees)/float(suma)
                     selected_node=choice(nodes, 1, p=degrees)[0]
-                    print(type(selected_node))
                     if not g.has_edge(selected_node,node_to_add):
                         g.add_edge(selected_node, node_to_add)
                         break
         #SHOW GRAPH DATA
-        pos=nx.random_layout(g) 
+        pos=nx.spring_layout(g) 
         nx.draw(g,pos,node_size=factor/N)
         plt.show()
         print(g.degree())
